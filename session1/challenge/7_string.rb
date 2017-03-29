eaf7ee1734e6da88dfffbe4642dba5e114c37ed1
Unai Motriko <unaimotriko@gmail.com>
2017-03-29 17:37:13 +0100

    Exercise file 1:7

--- session1/challenge/7_string.rb
+++ session1/challenge/7_string.rb
@@ -1,8 +1,13 @@
 # given a string, return the character after every letter "r"
-# 
+#
 # pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
 # pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+  new_string = String.new
+  for n in 0...string.length-1 do
+    new_string << string[n+1] if string[n] == "r" || string[n] == "R"
+  end
+  return new_string
 end
